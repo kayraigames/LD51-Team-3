59f477cc3a8d4f2a259e451394ac583b745fc70e
DanielR1 <activatingredstone@gmail.com>
2022-10-02 01:00:16 -0700

    flash timer

---
+++ LD51-Team-3/Assets/Flash.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash : MonoBehaviour
+{
+   Rigidbody2D rb;
+    // Start is called before the first frame update
+   // private System.Timers.Timer timer;
+    private Stopwatch timer;
+    private void Start()
+    {
+       // timer = new (interval: 1000);
+       // timer.start();
+       rb = GetComponent<Rigidbody2D>();
+     //  Destroy(gameObject);
+       timer = new Stopwatch();
+       timer.Start();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (timer.Elapsed.Seconds > 10){
+            //rb.velocity= new Vector2(0, 10);
+            timer = new Stopwatch();
+            timer.Start();
+        }
+    }
+}
